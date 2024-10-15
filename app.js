4600a1ceaf338a36ed233cef4d77a055480f4925
johanfortus <fortusjohan@gmail.com>
2024-10-15 17:58:04 -0400

    responsive on mobile and added a scroll icon and project section

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const typed = new Typed(".multiple-text", {
-    strings: ['Computer Science Student'],
+    strings: ['Computer Science Student', 'Software Developer', 'Content Creator', 'Video Editor', 'Entrepreneur'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
