f68d4e9ce2541009ecce36b2b53ae58c0042575f
johanfortus <fortusjohan@gmail.com>
2024-10-16 16:10:58 -0400

    finished styling inputs in modal

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
+    
 
 const typed = new Typed(".multiple-text", {
     strings: ['Computer Science Student', 'Software Developer', 'Content Creator', 'Video Editor', 'Entrepreneur'],
