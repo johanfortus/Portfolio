737230a05406d9e5d8d49cf6bab69afb3ea5c10c
johanfortus <fortusjohan@gmail.com>
2024-10-16 14:49:10 -0400

    modal progress

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
 const typed = new Typed(".multiple-text", {
     strings: ['Computer Science Student', 'Software Developer', 'Content Creator', 'Video Editor', 'Entrepreneur'],
     typeSpeed: 100,
