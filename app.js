1fdfe594b32edd1f52e14d7d796c4cd3dded8385
johanfortus <fortusjohan@gmail.com>
2024-10-20 18:19:51 -0400

    implemented toggle dark/light mode button

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-    
+
 
 const typed = new Typed(".multiple-text", {
     strings: ['Computer Science Student', 'Software Developer', 'Content Creator', 'Video Editor', 'Entrepreneur'],
@@ -7,3 +7,16 @@ const typed = new Typed(".multiple-text", {
     backDelay: 1000,
     loop: true
 });
+
+let html = document.querySelector("html");
+
+
+const modeButton = document.querySelector(".mode-button");
+modeButton.addEventListener("click", (e) => {
+    if(html.getAttribute("data-bs-theme") === "light") {
+        html.setAttribute("data-bs-theme", "dark");
+    }
+    else {
+        html.setAttribute("data-bs-theme", "light");
+    }
+})
