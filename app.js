a17713718b2d4e4f5e746116bc9fa1da6794a0b3
johanfortus <fortusjohan@gmail.com>
2024-12-12 23:52:16 -0500

    dark mode

--- app.js
+++ app.js
@@ -9,14 +9,29 @@ const typed = new Typed(".multiple-text", {
 });
 
 let html = document.querySelector("html");
+let navLink = document.querySelector(".nav-link");
 
+let aboutLink = document.getElementById("nav-about");
+let projectLink = document.getElementById("nav-projects");
+let contactLink = document.getElementById("nav-contact");
+let buttonLink = document.getElementById("nav-button");
 
 const modeButton = document.querySelector(".mode-button");
 modeButton.addEventListener("click", (e) => {
     if(html.getAttribute("data-bs-theme") === "light") {
         html.setAttribute("data-bs-theme", "dark");
+        
+        aboutLink.classList.toggle("nav-light")
+        projectLink.classList.toggle("nav-light")
+        contactLink.classList.toggle("nav-light")
+        buttonLink.classList.toggle("nav-light")
     }
     else {
         html.setAttribute("data-bs-theme", "light");
+
+        aboutLink.classList.toggle("nav-light")
+        projectLink.classList.toggle("nav-light")
+        contactLink.classList.toggle("nav-light")
+        buttonLink.classList.toggle("nav-light")
     }
 })
