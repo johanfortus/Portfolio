dc47a0fe62a7f00def7c35fc6ed347a95ab63d4e
johanfortus <fortusjohan@gmail.com>
2024-12-20 16:06:01 -0500

    adjusted grammer for hero subheading text

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 const typed = new Typed(".multiple-text", {
-    strings: ['Computer Science Student', 'Software Developer', 'Content Creator', 'Video Editor', 'Entrepreneur'],
+    strings: ['a Computer Science Student', 'a Software Developer', 'a Content Creator', 'a Video Editor', 'an Entrepreneur'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
